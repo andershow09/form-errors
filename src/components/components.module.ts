8b38c348c9547f4eb3bbd7a11d03c5b08c8d358a
Anderson <ander.ratm@gmail.com>
2018-01-13 20:16:57 -0200

    update  version

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FormErrorsComponent } from './form-errors/form-errors';
+@NgModule({
+	declarations: [FormErrorsComponent],
+	imports: [],
+	exports: [FormErrorsComponent]
+})
+export class ComponentsModule {}
